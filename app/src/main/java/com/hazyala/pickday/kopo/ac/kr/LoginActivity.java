3e52b9b29972904d4a8e9506747c409af11e1834
hazyala <heamin0603@naver.com>
2026-05-12 22:22:01 +0900

    loginActivity.java 생성, SplachActivity.java 수정

--- app/src/main/java/com/hazyala/pickday/kopo/ac/kr/LoginActivity.java
+++ app/src/main/java/com/hazyala/pickday/kopo/ac/kr/LoginActivity.java
@@ -1,24 +1,29 @@
 package com.hazyala.pickday.kopo.ac.kr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // 버튼 연결
+        btnStart = findViewById(R.id.btnStart);
+
+        // 버튼 클릭 이벤트
+        btnStart.setOnClickListener(v -> {
+
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+
         });
     }
 }
--- app/src/main/java/com/hazyala/pickday/kopo/ac/kr/SplashActivity.java
+++ app/src/main/java/com/hazyala/pickday/kopo/ac/kr/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.hazyala.pickday.kopo.ac.kr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -7,16 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    // 스플래시 유지 시간 (2초)
+    private static final int SPLASH_TIME = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                finish();
-            }
-        }, 2500);
+        // 일정 시간 후 LoginActivity로 이동
+        new Handler().postDelayed(() -> {
+
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+
+            // 스플래시 종료
+            finish();
+
+        }, SPLASH_TIME);
     }
 }
